8978d63161dc5fb82d579ad8fd72e0b83569c084
gaurav067sharma-creator <gaurav067sharma@gmail.com>
2026-04-24 22:59:18 +0000

    chat

--- frontend/src/ws.js
+++ frontend/src/ws.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
 export function connectWS() {
-    return io('https://symmetrical-memory-wv79p7494wgg2g94w-4600.app.github.dev/');
+    return io(import.meta.env.VITE_BACKEND_URL);
 }
